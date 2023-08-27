ac09c91be9a2106eca6bf8f3b542e26556cd2d12
Sofya <vanieva.sofya@mail.ru>
2023-08-27 09:07:40 +0300

    Yes

--- Task047/Program.cs
+++ Task047/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int m = 3;
+        int n = 4;
+        
+        double[,] array = new double[m, n];
+        
+        Random random = new Random();
+        
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                array[i, j] = random.NextDouble() * 20 - 10; 
+            }
+        }
+        
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(array[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
